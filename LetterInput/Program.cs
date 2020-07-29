9b8806cdc4522b2e6fda9447669c5ce52224fe22
Liza Star <lizadstar18@gmail.com>
2020-07-29 18:44:59 -0500

    got iteration to work to print letters in loops

--- LetterInput/Program.cs
+++ LetterInput/Program.cs
@@ -11,19 +11,18 @@ namespace LetterInput
             Console.WriteLine("Please type any word!");
 
             var userWord = Console.ReadLine();
-
-            //Console.WriteLine($"{userWord}");
-
+            //takes user input and sets it as and variable
             char[] characterArray = userWord.ToCharArray();
-
+            //takes input and sets it to an array of individual characters
             var endCharArr = characterArray.Select(c => c.ToString()).ToArray();
+            //makes copy of array to final result of each iteration of character to
             for (int i = 0; i < characterArray.Length; i++)
             {
                 var index = i;
                 var character = string.Join("", Enumerable.Repeat(characterArray[i], i + 1));
                 endCharArr[i] = FirstLetterCapital(character);
             }
-
+            //loops over each character and prints it into end result array and then use Enurerable and repeat i+1 to increase the amount of time the next letter in the array prints out
             static string FirstLetterCapital(string word)
             {
                 if (string.IsNullOrEmpty(word))
@@ -34,6 +33,7 @@ namespace LetterInput
                 a[0] = char.ToUpper(a[0]);
                 return new string(a);
             }
+            //returns string with capital letter at the beginning
             Console.WriteLine($"Your word iterated is: " + string.Join('-', endCharArr));
         }
     }
