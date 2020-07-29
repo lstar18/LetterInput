9d8d4009c19be48e628f6253974fe9329665eb8a
Liza Star <lizadstar18@gmail.com>
2020-07-29 18:36:04 -0500

    Add project files.

---
+++ LetterInput/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+
+namespace LetterInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type any word!");
+
+            var userWord = Console.ReadLine();
+
+            //Console.WriteLine($"{userWord}");
+
+            char[] characterArray = userWord.ToCharArray();
+
+            var endCharArr = characterArray.Select(c => c.ToString()).ToArray();
+            for (int i = 0; i < characterArray.Length; i++)
+            {
+                var index = i;
+                var character = string.Join("", Enumerable.Repeat(characterArray[i], i + 1));
+                endCharArr[i] = FirstLetterCapital(character);
+            }
+
+            static string FirstLetterCapital(string word)
+            {
+                if (string.IsNullOrEmpty(word))
+                {
+                    return string.Empty;
+                }
+                char[] a = word.ToCharArray();
+                a[0] = char.ToUpper(a[0]);
+                return new string(a);
+            }
+            Console.WriteLine($"Your word iterated is: " + string.Join('-', endCharArr));
+        }
+    }
+}
